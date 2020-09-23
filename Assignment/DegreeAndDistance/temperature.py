50430c721c88f9ff0e5f74c649a2faa678ad8e91
yg19d <yg19d@my.fsu.edu>
2020-09-22 22:27:52 -0400

    Create temperature.py

---
+++ Assignment/DegreeAndDistance/temperature.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# link to the repository- github.com/yg19d/BCH5884
+
+import math
+
+x=float(input("Please enter the value of farenheit you want to convert in kelvin: "))
+
+y=273.15 + ((x-32)*(5/9))
+
+print ("Temperature converted temperature in degree kelvin is", y)
+
